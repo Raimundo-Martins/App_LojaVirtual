a36dcf83595ecbb16226d898695a4a249d1df5f8
Raimundo-Martins <cosmomartinsj@gmail.com>
2021-01-30 19:58:56 -0300

    Atualizando

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
@@ -17,6 +18,50 @@ class _ProductScreenState extends State<ProductScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(productData.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images:
+                  productData.images.map((url) => NetworkImage(url)).toList(),
+              dotSize: 4,
+              dotSpacing: 15,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              autoplay: true,
+              autoplayDuration: Duration(seconds: 3),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  productData.title,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  maxLines: 3,
+                ),
+                Text(
+                  'R\$ ${productData.price.toStringAsFixed(2)}',
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
